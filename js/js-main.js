da738f1a0f7e3ded8c579631f8bb5360c956a2c9
Isaac Scheeweis <isaac.fagerli@gmail.com>
2018-03-31 18:23:01 +0200

    yeaa

--- js/js-main.js
+++ js/js-main.js
@@ -1,41 +1,59 @@
+//calculate full-width/height view
+const visibleHeightAtZDepth = ( depth, camera ) => {
+  // compensate for cameras not positioned at z=0
+  const cameraOffset = camera.position.z;
+  if ( depth < cameraOffset ) depth -= cameraOffset;
+  else depth += cameraOffset;
+
+  // vertical fov in radians
+  const vFOV = camera.fov * Math.PI / 180; 
+
+  // Math.abs to ensure the result is always positive
+  return 2 * Math.tan( vFOV / 2 ) * Math.abs( depth );
+};
+
+const visibleWidthAtZDepth = ( depth, camera ) => {
+  const height = visibleHeightAtZDepth( depth, camera );
+  return height * camera.aspect;
+};
 
 
-//Y = (X-A)/(B-A) * (D-C) + C
-// X = input, Y = Output, A-B = Input range, D-C = Output range
 
-var uu;
-function setup() {
-    
-    createCanvas(window.innerWidth, window.innerHeight,WEBGL);
+//set the renderer
+var csrenderer = new THREE.CSS3DRenderer();
+document.body.appendChild( csrenderer.domElement );
+csrenderer.setSize(window.innerWidth, window.innerHeight);
 
-    
-}
 
-function draw() {
-    rotateAmnt = map(mouseX,0,window.innerWidth,0,90);
-    background(255);
-    fill(255);
-    //strokeWeight(2);
-   // stroke(51,0,255);
-    angleMode(DEGREES);
-    rectMode(CENTER);
-    translate(0,0,(window.innerWidth/2)*-1);
-    //translate(-window.innerWidth/2, -window.innerHeight/2);
-    rotateY(-rotateAmnt);
-   
-    push();
-    ambientMaterial(250);
-    translate(0,0,window.innerWidth/2);
-    rect(0,0,window.innerWidth,window.innerHeight);
-    pop();
-    
-    push();
-    specularMaterial(250);
-    translate(window.innerWidth/2,0,0);
-    rotateY(90);
-    rect(0,0,window.innerWidth,window.innerHeight);
-    pop();
-    
-    pointLight(255,255,255,0,0,1440);
-   
-}
+//camera
+var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 3000);
+camera.position.set( 0, 0, 20 );
+
+//scene
+var scene = new THREE.Scene();
+
+//grab an HTML element, set styling
+var domElement = document.getElementById("introtext");
+domElement.style.width = visibleWidthAtZDepth( -500, camera) + "px";
+console.log(visibleHeightAtZDepth( 10000000, camera));
+domElement.style.height = visibleHeightAtZDepth( -500, camera) + "px";
+
+
+//store HTML element in variable, set position
+obj = new THREE.CSS3DObject(domElement);
+obj.position.z = -500;
+
+
+
+
+
+//add camera and HTML element to scene
+scene.add(obj, camera);
+
+//Push to renderer
+csrenderer.render(scene, camera);
+
+
+
+
+//https://stackoverflow.com/questions/15214582/how-do-i-rotate-some-moons-around-a-planet-with-three-js
