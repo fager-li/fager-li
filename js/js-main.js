93289f401038e39b089ec1fe6fc225c8af0fa017
Isaac Scheeweis <isaac.fagerli@gmail.com>
2018-04-01 15:27:13 +0200

    getting there

--- js/js-main.js
+++ js/js-main.js
@@ -19,11 +19,14 @@ const visibleWidthAtZDepth = ( depth, camera ) => {
 
 
 
+//initial variables
+var parent, plane1, plane2, plane3,plane4, pivot1, pivot2, pivot3, pivot4;
+var filler = document.getElementById('filler');
+var zdepth = -1500;
 //set the renderer
 var csrenderer = new THREE.CSS3DRenderer();
 document.body.appendChild( csrenderer.domElement );
 csrenderer.setSize(window.innerWidth, window.innerHeight);
-
 
 //camera
 var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.1, 3000);
@@ -32,25 +35,80 @@ camera.position.set( 0, 0, 20 );
 //scene
 var scene = new THREE.Scene();
 
+//create parent
+parent = new THREE.CSS3DObject(filler);
+scene.add(parent);
+parent.position.z = zdepth-visibleWidthAtZDepth(zdepth, camera)/2;
+
+
+
+
+//Create pivots
+var pivot1 = new THREE.CSS3DObject(filler);
+var pivot2 = new THREE.CSS3DObject(filler);
+var pivot3 = new THREE.CSS3DObject(filler);
+var pivot4 = new THREE.CSS3DObject(filler);
+
+pivot1.rotation.y = 0;
+pivot2.rotation.y = 1.5708;
+pivot3.rotation.y = 3.14159;
+pivot4.rotation.y = 4.71239;
+
+parent.add(pivot1);
+parent.add(pivot2);
+parent.add(pivot3);
+parent.add(pivot4);
+
 //grab an HTML element, set styling
 var domElement = document.getElementById("introtext");
-domElement.style.width = visibleWidthAtZDepth( -500, camera) + "px";
-console.log(visibleHeightAtZDepth( 10000000, camera));
-domElement.style.height = visibleHeightAtZDepth( -500, camera) + "px";
+domElement.style.width = visibleWidthAtZDepth( zdepth, camera) + "px";
+
+domElement.style.height = visibleHeightAtZDepth( zdepth, camera) + "px";
+
+var domElement2 = document.getElementById("introtext2");
+domElement2.style.width = visibleWidthAtZDepth( zdepth, camera) + "px";
+console.log(visibleWidthAtZDepth( zdepth, camera));
+domElement2.style.height = visibleHeightAtZDepth( zdepth, camera) + "px";
+
+var domElement3 = document.getElementById("introtext3");
+domElement3.style.width = visibleWidthAtZDepth( zdepth, camera) + "px";
+domElement3.style.height = visibleHeightAtZDepth( zdepth, camera) + "px";
+
+var domElement4 = document.getElementById("introtext4");
+domElement4.style.width = visibleWidthAtZDepth( zdepth, camera) + "px";
+domElement4.style.height = visibleHeightAtZDepth( zdepth, camera) + "px";
 
 
 //store HTML element in variable, set position
-obj = new THREE.CSS3DObject(domElement);
-obj.position.z = -500;
+plane1 = new THREE.CSS3DObject(domElement);
+plane2 = new THREE.CSS3DObject(domElement2);
+plane3 = new THREE.CSS3DObject(domElement3);
+plane4 = new THREE.CSS3DObject(domElement4);
 
+plane1.position.z = visibleWidthAtZDepth(zdepth, camera)/2;
+plane2.position.z = visibleWidthAtZDepth(zdepth, camera)/2;
+plane3.position.z = visibleWidthAtZDepth(zdepth, camera)/2;
+plane4.position.z = visibleWidthAtZDepth(zdepth, camera)/2;
+
+//add HTML Elements to pivots
+pivot1.add(plane1);
+pivot2.add(plane2);
+pivot3.add(plane3);
+pivot4.add(plane4);
 
 
 //add camera and HTML element to scene
-scene.add(obj, camera);
+scene.add(parent, camera);
 
 //Push to renderer
-csrenderer.render(scene, camera);
+render();
+function render() {
+	
+    //parent.rotation.y += 0.01;
+    csrenderer.render(scene, camera);
 
+	requestAnimationFrame(render);
+}
 
 
 
