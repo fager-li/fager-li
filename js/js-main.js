a0de67b3b81c10b057df474ed07cf704deb3706a
Isaac Schneeweis <isaac.schneeweis@Xadmins-MacBook-Pro-3.local>
2019-01-25 09:59:59 +0100

    heh

--- js/js-main.js
+++ js/js-main.js
@@ -74,31 +74,16 @@ var slowingFactor = 0.25;
 var gn = new GyroNorm();
 
 gn.init().then(function(){
-  gn.start(function(data){
-    // Process:
-    // data.do.alpha	( deviceorientation event alpha value )
-    // data.do.beta		( deviceorientation event beta value )
-    // data.do.gamma	( deviceorientation event gamma value )
-    // data.do.absolute	( deviceorientation event absolute value )
-      document.getElementById("testingthis").innerHTML = data.do.gamma;
-    // data.dm.x		( devicemotion event acceleration x value )
-    // data.dm.y		( devicemotion event acceleration y value )
-    // data.dm.z		( devicemotion event acceleration z value )
-
-    // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
-    // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
-    // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
-
-    // data.dm.alpha	( devicemotion event rotationRate alpha value )
-    // data.dm.beta		( devicemotion event rotationRate beta value )
-    // data.dm.gamma	( devicemotion event rotationRate gamma value )
+    start_gn();
   });
-}).catch(function(e){
-  // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-});
+
+    function start_gn() {
+      gn.start(gnCallBack);
+    }
 //gn stop
-
-
+function gnCallBack(data) {
+ document.getElementById("testingthis").innerHTML = data.do.gamma;
+}
 //click-and-drag for desktop
 document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 
